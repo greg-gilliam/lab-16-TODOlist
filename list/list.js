9d1c89cdd1aa06f0ef26e3fc986fdf122b65ba23
Paul Brubaker <pbrubak@gmail.com>
2021-07-23 17:15:58 -0700

    works

--- list/list.js
+++ list/list.js
@@ -4,25 +4,28 @@ import { findByName } from '../utils/helpers.js';
 const newNote = document.getElementById('newNote');
 const submitNewNote = document.getElementById('submitNewNote');
 const searchParams = new URLSearchParams(window.location.search);
+const ulContainer = document.getElementById('notesContainer');
 
 let user = findByName(getUsers(), (searchParams.get('name')));
-renderNotes(user);
+ulContainer.innerHTML = '';
+ulContainer.appendChild(renderNotes(user));
 
 submitNewNote.addEventListener('click', () =>{
     let user = findByName(getUsers(), (searchParams.get('name')));
     updateToDos(user, newNote.value, false);
     user = findByName(getUsers(), (searchParams.get('name')));
-    renderNotes(user);
+    ulContainer.innerHTML = '';
+    ulContainer.appendChild(renderNotes(user));
 });
 
 function updateToDos(user, newNote, bool) {
+    const updatedTodos = user.todos.filter(item => item.name !== newNote);
     const updatedUser = {
         id: user.id, 
         name: user.name,
         password: user.password,
-        todos: [...user.todos, { name:newNote, completed:bool }],
+        todos: [...updatedTodos, { name: newNote, completed:bool }],
     };
-
     const users = getUsers().filter(item => item.name !== user.name);
     users.push(updatedUser);
     setUsers(users);
@@ -30,7 +33,6 @@ function updateToDos(user, newNote, bool) {
 }
 
 function renderNotes(user) {
-    const ulContainer = document.getElementById('notesContainer');
     const ul = document.createElement('ul');
     for (let listItem of user.todos) {
         const span = document.createElement('span');
@@ -39,20 +41,19 @@ function renderNotes(user) {
         li.append(button, span);
         ul.appendChild(li);
         span.textContent = listItem.name;
-        console.log(listItem.completed);
-        if (listItem.completed === false){
-            button.addEventListener('click', ()=>{
-                updateListItemCompleted(user, listItem);
-                const updatedUser = findByName(getUsers(), user.name);
-                console.log(user);
-                renderNotes(updatedUser);
-            });
-        } else {
-            button.textContent = 'X';
+        if (listItem.completed === true) {
+            button.innerHTML = 'X';
             span.style.textDecoration = 'line-through';
         }
+        button.addEventListener('click', () => {
+            if (listItem.completed === false) {
+                button.innerHTML = 'X';
+                span.style.textDecoration = 'line-through';
+                updateListItemCompleted(user, listItem.name);
+            }
+        });
     }
-    ulContainer.innerHTML = ul.outerHTML;
+    return ul;
 } 
 
 function updateListItemCompleted(user, listItem) {
